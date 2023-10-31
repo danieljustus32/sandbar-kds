18008828609c9b609c5a2c62af23031fbc5f5763
Daniel Justus <daniel.justus32@gmail.com>
2023-10-31 01:13:43 -0700

    Add env variables for Plasmic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
+import { React, useEffect } from "react";
 
 function App() {
+  // Find your CMS ID and Public Token from the settings page.
+  const CMS_ID = process.env.REACT_APP_PLASMIC_CMS_ID;
+  const CMS_PUBLIC_TOKEN = process.env.REACT_APP_PLASMIC_PUBLIC_KEY;
+
+  useEffect(() => {
+    console.log(CMS_ID);
+    console.log(CMS_PUBLIC_TOKEN);
+  });
+
   return (
     <div className="App">
       <header className="App-header">No Online Orders!</header>
