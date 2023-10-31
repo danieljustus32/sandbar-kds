46ca410c0723a43288608a771412a4f27d0b7b65
Daniel Justus <daniel.justus32@gmail.com>
2023-10-31 15:42:33 -0700

    Add loading state, loading indicator, query interval

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import "./App.css";
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
+import { ScaleLoader } from "react-spinners";
 
 function App() {
   // Find your CMS ID and Public Token from the settings page.
   const CMS_ID = process.env.REACT_APP_PLASMIC_CMS_ID;
   const CMS_PUBLIC_TOKEN = process.env.REACT_APP_PLASMIC_PUBLIC_KEY;
 
+  const [isBusy, setIsBusy] = useState(false);
+
   useEffect(() => {
-    console.log(CMS_ID);
-    console.log(CMS_PUBLIC_TOKEN);
-  });
+    setInterval(() => {
+      setIsBusy(true);
+      console.log("KDS is checking for tickets!");
+      // Do network stuff
+      setIsBusy(false);
+    }, 15000);
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">No Online Orders!</header>
+      <header className="App-header">
+        No Online Orders! {isBusy && <ScaleLoader color="#fff" />}
+      </header>
     </div>
   );
 }
