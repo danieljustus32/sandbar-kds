671a522f22d6005f5d5b53f482d7c6db1d3cfd59
Daniel Justus <daniel.justus32@gmail.com>
2023-11-01 04:24:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ function App() {
   // Find your CMS ID and Public Token from the settings page.
   const CMS_ID = process.env.REACT_APP_PLASMIC_CMS_ID;
   const CMS_PUBLIC_TOKEN = process.env.REACT_APP_PLASMIC_PUBLIC_KEY;
+  const CMS_SECRET_TOKEN = process.env.REACT_APP_PLASMIC_SECRET_KEY;
   const [openTickets, setOpenTickets] = useState([]);
 
   const modelId = "order";
 
-  function formatPhoneNumber(phoneNumberString) {
-    var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
-    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
-    if (match) {
-      return "(" + match[1] + ") " + match[2] + "-" + match[3];
-    }
-    return null;
-  }
+  // function formatPhoneNumber(phoneNumberString) {
+  //   var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+  //   var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  //   if (match) {
+  //     return "(" + match[1] + ") " + match[2] + "-" + match[3];
+  //   }
+  //   return null;
+  // }
 
   useEffect(() => {
+    // Main logic for pulling down the oldest 4 open tickets from Plasmic CMS //
     setInterval(async () => {
       const apiUrl = new URL(
         `https://data.plasmic.app/api/v1/cms/databases/${CMS_ID}/tables/${modelId}/query`
@@ -43,7 +45,32 @@ function App() {
         alert(error);
       }
     }, 1000);
-  }, [CMS_ID, CMS_PUBLIC_TOKEN]);
+  }, []);
+
+  async function closeTicket(rowId) {
+    const apiUrl = new URL(`https://data.plasmic.app/api/v1/cms/rows/${rowId}`);
+    // Do network stuff
+    try {
+      const updateJson = {
+        data: {
+          open: false,
+        },
+      };
+      const response = await fetch(apiUrl.toString(), {
+        method: "PUT",
+        headers: {
+          // Plasmic CMS ID and CMS Public API token
+          "x-plasmic-api-cms-tokens": `${CMS_ID}:${CMS_SECRET_TOKEN}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(updateJson),
+      });
+      const parsedResponse = await response.json();
+      console.log(parsedResponse);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   // FOR DEBUG PURPOSES //
   useEffect(() => {
@@ -67,9 +94,10 @@ function App() {
               {openTickets.map((ticket) => {
                 return (
                   <div key={ticket.id} className="ticket">
-                    <span>{ticket.data.firstName}</span>
+                    <span>{ticket.data.lastName}</span>
                     <span>{lpad(ticket.data.number, 3, "0")}</span>
                     <span>{moment(ticket.createdAt).format("HH:mm")}</span>
+                    <button onClick={() => closeTicket(ticket.id)}></button>
                     <div className="order-wrapper">
                       <ul>
                         {ticket.data.order.map((entry) => {
